957880818c0579a515289dab1e140cc621d32cb4
JonasZarnhofer <joza782@gmail.com>
2025-02-24 11:29:49 +0100

    submission schema

--- frontend/src/lib/components/SubmissionForm.svelte
+++ frontend/src/lib/components/SubmissionForm.svelte
@@ -9,6 +9,7 @@
 	import { type SuperValidated, type Infer, superForm } from 'sveltekit-superforms';
 	import { zodClient } from 'sveltekit-superforms/adapters';
 	import { submissionSchema, type SubmissionSchema } from '$lib/schemas';
+	import { Fieldset } from 'formsnap';
 
 	let { data }: { data: { form: SuperValidated<Infer<SubmissionSchema>> } } = $props();
 	const form = superForm(data.form, {
@@ -31,60 +32,58 @@
 		<div class="flex w-full justify-between items-center">
 			<div class="flex flex-col space-y-4 w-1/2 justify-center items-start">
 				<Form.Field {form} name="start" class="w-full">
-					<Form.Control>
+					<Form.Control let:attrs>
 						{#snippet children({ props })}
 							<Form.Label>Start Date</Form.Label>
 							<Input
-								{...props}
 								type="date"
 								class="bg-slate-600 border-slate-500"
 								bind:value={$formData.start}
 							/>
 						{/snippet}
 					</Form.Control>
+					<Form.FieldErrors />
 				</Form.Field>
 
 				<Form.Field {form} name="end" class="w-full">
-					<Form.Control>
+					<Form.Control let:attrs>
 						{#snippet children({ props })}
 							<Form.Label>End Date</Form.Label>
-							<Input
-								{...props}
-								type="date"
-								class="bg-slate-600 border-slate-500"
-								bind:value={$formData.end}
-							/>
+							<Input type="date" class="bg-slate-600 border-slate-500" bind:value={$formData.end} />
 						{/snippet}
 					</Form.Control>
+					<Form.FieldErrors />
 				</Form.Field>
 			</div>
 			<div class="flex flex-col space-y-4 justify-center items-start w-1/3">
 				<Form.Field {form} name="total_costs" class="w-full">
-					<Form.Control>
+					<Form.Control let:attrs>
 						{#snippet children({ props })}
-							<Form.Label>Total Costs</Form.Label>
+							<Form.Label for="total">Total Costs</Form.Label>
 							<Input
-								{...props}
 								type="number"
+								name="total"
 								class="bg-slate-600 border-slate-500"
 								bind:value={$formData.total_costs}
 							/>
 						{/snippet}
 					</Form.Control>
+					<Form.FieldErrors />
 				</Form.Field>
 
 				<Form.Field {form} name="transportation_costs" class="w-full">
-					<Form.Control>
+					<Form.Control let:attrs>
 						{#snippet children({ props })}
 							<Form.Label>Transportation Costs</Form.Label>
 							<Input
-								{...props}
 								type="number"
+								name="transportation"
 								class="bg-slate-600 border-slate-500"
 								bind:value={$formData.transportation_costs}
 							/>
 						{/snippet}
 					</Form.Control>
+					<Form.FieldErrors />
 				</Form.Field>
 			</div>
 		</div>
@@ -93,56 +92,68 @@
 
 		<div class="flex flex-col space-y-4 w-full justify-between items-center">
 			<h2 class="h2 items-end w-full text-2xl font-bold">Participants</h2>
-			<div class="flex space-x-4 w-full justify-start items-center">
-				<fieldset>
-					{#each $formData.teachers as teacher, index}
-						<Input type="text" bind:value={$formData.teachers[index]} />
-					{/each}
-					<Button
-						type="button"
-						class="bg-slate-600 text-white text-lg p-6 hover:bg-slate-500"
-						on:click={() => $formData.teachers.push('')}>Add Teacher</Button
-					>
-				</fieldset>
-				<fieldset>
-					{#each $formData.students as student, index}
-						<Input type="text" bind:value={$formData.students[index]} />
-					{/each}
-					<Button
-						type="button"
-						class="bg-slate-600 text-white text-lg p-6 hover:bg-slate-500"
-						on:click={() => $formData.students.push('')}>Add Student</Button
-					>
-				</fieldset>
+			<div class="flex space-x-4 w-full justify-start items-start">
+				<Form.Fieldset {form} name="teachers">
+					<Form.Control let:attrs>
+						{#snippet children({ props })}
+							{#each $formData.teachers as teacher, index}
+								<Input type="text" bind:value={$formData.teachers[index]} />
+							{/each}
+							<Button
+								{...props}
+								type="button"
+								class="bg-slate-600 text-white text-lg p-6 hover:bg-slate-500"
+								on:click={() => $formData.teachers.push('')}>Add Teacher</Button
+							>
+						{/snippet}
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Fieldset>
+
+				<Form.Fieldset {form} name="students">
+					<Form.Control let:attrs>
+						{#snippet children({ props })}
+							{#each $formData.students as student, index}
+								<Input type="text" bind:value={$formData.students[index]} />
+							{/each}
+							<Button
+								{...props}
+								type="button"
+								class="bg-slate-600 text-white text-lg p-6 hover:bg-slate-500"
+								on:click={() => $formData.students.push('')}>Add Student</Button
+							>
+						{/snippet}
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Fieldset>
 			</div>
 		</div>
 
 		<Separator class="bg-slate-600 w-2/3" />
 
 		<h2 class="h2 items-end w-full text-2xl font-bold">Address</h2>
-		<fieldset class="flex flex-col w-full space-y-3">
+		<div class="flex flex-col w-full space-y-3">
 			<div class="flex w-full justify-between items-start space-x-3">
 				<div class="flex flex-col space-y-2 justify-center items-start w-1/2">
-					<Form.Field {form} name="address" class="w-full">
-						<Form.Control>
+					<Form.Field {form} name="address.country" class="w-full">
+						<Form.Control let:attrs>
 							{#snippet children({ props })}
 								<Form.Label>Country</Form.Label>
 								<Input
-									{...props}
 									type="text"
 									class="bg-slate-600 border-slate-500"
 									bind:value={$formData.address.country}
 								/>
 							{/snippet}
 						</Form.Control>
+						<Form.FieldErrors />
 					</Form.Field>
 
-					<Form.Field {form} name="address" class="w-full">
-						<Form.Control>
+					<Form.Field {form} name="address.state" class="w-full">
+						<Form.Control let:attrs>
 							{#snippet children({ props })}
 								<Form.Label>State</Form.Label>
 								<Input
-									{...props}
 									type="text"
 									class="bg-slate-600 border-slate-500"
 									bind:value={$formData.address.state}
@@ -151,63 +162,80 @@
 						</Form.Control>
 					</Form.Field>
 				</div>
+
 				<div class="flex flex-col space-y-2 justify-center items-start w-1/2">
-					<Form.Field {form} name="address" class="w-full">
-						<Form.Control>
+					<Form.Field {form} name="address.street" class="w-full">
+						<Form.Control let:attrs>
 							{#snippet children({ props })}
 								<Form.Label>City</Form.Label>
 								<Input
 									type="text"
-									{...props}
 									class="bg-slate-600 border-slate-500"
 									bind:value={$formData.address.street}
 								/>
 							{/snippet}
 						</Form.Control>
+						<Form.FieldErrors />
 					</Form.Field>
 
-					<Form.Field {form} name="address" class="w-full">
-						<Form.Control>
+					<Form.Field {form} name="address.street" class="w-full">
+						<Form.Control let:attrs>
 							{#snippet children({ props })}
 								<Form.Label>Street</Form.Label>
 								<Input
 									type="text"
-									{...props}
 									class="bg-slate-600 border-slate-500"
 									bind:value={$formData.address.street}
 								/>
 							{/snippet}
 						</Form.Control>
+						<Form.FieldErrors />
 					</Form.Field>
 
-					<Form.Field {form} name="address" class="w-full">
-						<Form.Control>
+					<Form.Field {form} name="address.zip" class="w-full">
+						<Form.Control let:attrs>
 							{#snippet children({ props })}
 								<Form.Label>Zip</Form.Label>
 								<Input
-									{...props}
 									type="text"
 									class="bg-slate-600 border-slate-500"
-									bind:value={$formData.address.city}
+									bind:value={$formData.address.zip}
 								/>
 							{/snippet}
 						</Form.Control>
+						<Form.FieldErrors />
 					</Form.Field>
 				</div>
 			</div>
-		</fieldset>
+		</div>
 
 		<Separator class="bg-slate-600 w-2/3" />
 
-		<Label>Curriculum Reference</Label>
-		<Textarea
-			type="text"
-			bind:value={$formData.curriculum_ref}
-			class=" bg-slate-600 border-slate-500"
-		/>
+		<Form.Field {form} name="curriculum_ref" class="w-full">
+			<Form.Control let:attrs>
+				{#snippet children({ props })}
+					<Form.Label>Curriculum Reference</Form.Label>
+					<Textarea
+						bind:value={$formData.curriculum_ref}
+						class=" bg-slate-600 border-slate-500 h-36"
+					/>
+				{/snippet}
+			</Form.Control>
+			<Form.FieldErrors />
+		</Form.Field>
 
-		<Label>Parental Info</Label>
-		<Textarea class="bg-slate-600 border-slate-500" bind:value={$formData.parental_info} />
+		<Form.Field {form} name="parental_info" class="w-full">
+			<Form.Control let:attrs>
+				{#snippet children({ props })}
+					<Form.Label>Parental Info</Form.Label>
+					<Textarea
+						class="bg-slate-600 border-slate-500 h-36"
+						bind:value={$formData.parental_info}
+					/>
+				{/snippet}
+			</Form.Control>
+			<Form.FieldErrors />
+		</Form.Field>
 
 		<div class="flex justify-end w-full pt-5">
 			<Form.Button class="bg-purple-700 text-white text-lg p-6 hover:bg-purple-800"
--- frontend/src/lib/schemas.ts
+++ frontend/src/lib/schemas.ts
@@ -2,21 +2,29 @@
 import { z } from "zod";
 
 export const submissionSchema = z.object({
-    start: z.date(),
-    end: z.date(),
-    curriculum_ref: z.string(),
-    total_costs: z.number(),
-    transportation_costs: z.number(),
-    parental_info: z.string(),
-    teachers: z.array(z.string()),
-    students: z.array(z.string()),
+    start: z.string()
+        .transform((val) => new Date(val))
+        .refine((date) => !isNaN(date.getTime()), { message: "Invalid date" })
+        .refine((date) => date >= new Date(), { message: "Start date must be in the future" }),
+    end: z.string()
+        .transform((val) => new Date(val))
+        .refine((date) => !isNaN(date.getTime()), { message: "Invalid date" }),
+    curriculum_ref: z.string().nonempty(),
+    total_costs: z.number().positive(),
+    transportation_costs: z.number().positive(),
+    parental_info: z.string().nonempty(),
+    teachers: z.array(z.string()).length(1),
+    students: z.array(z.string()).length(1),
     address: z.object({
-        street: z.string(),
-        city: z.string(),
-        country: z.string(),
-        state: z.string(),
-        zip: z.string()
+        street: z.string().nonempty(),
+        city: z.string().nonempty(),
+        country: z.string().nonempty(),
+        state: z.string().nonempty(),
+        zip: z.string().nonempty()
     })
-});
+}).refine((schema) => {
+    console.log(schema.start, schema.end);
+    return schema.start < schema.end;
+}, "End must be after start date");
 
 export type SubmissionSchema = typeof submissionSchema;
