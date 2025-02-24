fa36c91dec69eb20cdc9c0f5c6453471080f9739
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2025-02-24 09:19:44 +0100

    Fixed post and patch routes for events and users

--- src/backend/crud/events.py
+++ src/backend/crud/events.py
@@ -1,6 +1,7 @@
 from typing import List
-
+from copy import copy
 from pydantic import BaseModel
+from fastapi import HTTPException
 from crud.crud import CRUD, log_db, select
 from sqlalchemy.sql import func
 from sqlalchemy.orm import aliased
@@ -60,7 +61,12 @@ class EventCRUD(CRUD[Event]):
         return event
     
     def patch(self, id, request: EventPatch) -> EventDB:
-        event = super().patch(id, request.model_dump(exclude={"teachers", "students", "address"})) 
+        unset_copy = copy(request)
+        unset_copy.teachers = None
+        unset_copy.students = None
+        unset_copy.address = None
+
+        event = super().patch(id, unset_copy) 
         if event is None:
             return None
 
@@ -73,11 +79,19 @@ class EventCRUD(CRUD[Event]):
         if request.students is not None:
             event.students = [students.get_by_id(student_id) for student_id in request.students]
 
+            if None in event.students:
+                self.session.rollback()
+                raise HTTPException(status_code=400, detail="Student not found")
+
         self.session.commit()
 
         # Update for multi-day event check
         # if request.end - request.start > timedelta(days=1):
         #    multi_day_events.create(event.event_id, None)
+        pending_approval = session.query(UserApproveEvent).where(and_(UserApproveEvent.event_id == event.event_id, UserApproveEvent.is_approved == False)).all()
+        event.pending_approval = pending_approval
+
+        session.refresh(event)
         return event
 
     def put(self, id, request):
